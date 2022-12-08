d2883d4bbcd10d0c76332fb7af0f0731528ebe83
Nick Conway <nick@conway.dev>
2022-12-07 20:32:57 -0500

    Add days

---
+++ 2022/10/main.py
@@ -0,0 +1,14 @@
+example = open("example.txt").read()
+input = open("input.txt").read()
+
+def part_one(file):
+    pass
+
+def part_two(file):
+    pass
+
+part_one(example)
+part_one(input)
+print()
+part_two(example)
+part_two(input)
